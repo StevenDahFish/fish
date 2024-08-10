a0be4b2ae74607e6bf90a542a1b913bf1f465faf
StevenDahFish <stevendahfish@gmail.com>
2024-08-10 14:45:44 -0700

    Built and published by Moonwave

---
+++ assets/js/3763247c.6f876289.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[959],{63891:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/StevenDahFish/fish/blob/master","baseUrl":"/fish/","classOrder":[],"apiCategories":[]}')}}]);
---
+++ assets/js/a4401732.a762101c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[239],{7636:e=>{e.exports=JSON.parse('[{"type":"link","href":"/api/Client","label":"\u200bClient"},{"type":"link","href":"/api/Server","label":"\u200bServer"},{"type":"link","href":"/api/Types","label":"\u200bTypes"}]')}}]);
